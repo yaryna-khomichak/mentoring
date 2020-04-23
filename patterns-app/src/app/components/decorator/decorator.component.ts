4fd10ce9806f9f1cd8aa00254b2acb3148e43d81
yarynalazurko <yaryna1603@gmail.com>
2020-04-24 00:18:41 +0300

    Feature: Implement decorator

--- patterns-app/src/app/components/decorator/decorator.component.ts
+++ patterns-app/src/app/components/decorator/decorator.component.ts
@@ -10,6 +10,31 @@ export class DecoratorComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    class Tea {
+      cost() {
+        return 5;
+      }
+    }
+
+    function sugar(tea) {
+      const cost = tea.cost();
+      tea.cost = () => cost + 1;
+    }
+
+    function honey(tea) {
+      const cost = tea.cost();
+      tea.cost = () => cost + 2;
+    }
+
+    function ment(tea) {
+      const cost = tea.cost();
+      tea.cost = () => cost + 2.5;
+    }
+
+    const tea = new Tea();
+    honey(tea);
+    ment(tea);
+    console.log(tea.cost());
   }
 
 }
