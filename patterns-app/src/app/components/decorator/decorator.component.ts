dcaf7bb3946cf75a1d9ad91ce43629a45d1293d0
yarynalazurko <yaryna1603@gmail.com>
2020-04-24 09:34:08 +0300

    Feature: Add decorator

--- patterns-app/src/app/components/decorator/decorator.component.ts
+++ patterns-app/src/app/components/decorator/decorator.component.ts
@@ -10,31 +10,48 @@ export class DecoratorComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    class Tea {
-      cost() {
-        return 5;
+    // class Tea {
+    //   cost() {
+    //     return 5;
+    //   }
+    // }
+
+    // function sugar(tea) {
+    //   const cost = tea.cost();
+    //   tea.cost = () => cost + 1;
+    // }
+
+    // function honey(tea) {
+    //   const cost = tea.cost();
+    //   tea.cost = () => cost + 2;
+    // }
+
+    // function ment(tea) {
+    //   const cost = tea.cost();
+    //   tea.cost = () => cost + 2.5;
+    // }
+
+    // const tea = new Tea();
+    // honey(tea);
+    // ment(tea);
+    // console.log(tea.cost());
+
+    function extraInfo(target: any) {
+      console.log('We decorated: ', target);
+    }
+
+    @extraInfo
+    class User {
+      name: string;
+      age: number;
+      constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
       }
     }
 
-    function sugar(tea) {
-      const cost = tea.cost();
-      tea.cost = () => cost + 1;
-    }
-
-    function honey(tea) {
-      const cost = tea.cost();
-      tea.cost = () => cost + 2;
-    }
-
-    function ment(tea) {
-      const cost = tea.cost();
-      tea.cost = () => cost + 2.5;
-    }
-
-    const tea = new Tea();
-    honey(tea);
-    ment(tea);
-    console.log(tea.cost());
+    const bob = new User('Bob', 20);
+    console.log(bob);
   }
 
 }
