724937dcd3daf7e6c23ea4904b350efd44cfdf5e
yarynalazurko <yaryna1603@gmail.com>
2020-05-03 11:29:11 +0300

    Add reactive form

--- patterns-app/src/app/components/registration-page/reactive-form/reactive-form.component.ts
+++ patterns-app/src/app/components/registration-page/reactive-form/reactive-form.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { RegistrationPageService } from 'src/app/services/registration-page.service';
+import { UserData } from 'src/app/models/user-data.class';
 
 @Component({
 	selector: 'app-reactive-form',
 	templateUrl: './reactive-form.component.html',
 	styleUrls: ['./reactive-form.component.scss']
 })
-export class ReactiveFormComponent implements OnInit {
+export class ReactiveFormComponent {
+	public myForm: FormGroup = new FormGroup({
+		email: new FormControl('', [
+			Validators.required,
+			Validators.email
+		]),
+		password: new FormControl('', Validators.required),
+		firstName: new FormControl('', Validators.required),
+		lastName: new FormControl('', Validators.required)
+	});
 
-	constructor() { }
+	constructor(private registrationPageService: RegistrationPageService) { }
 
-	ngOnInit() {
+	public addUser() {
+		this.registrationPageService.addUser(
+			new UserData(this.myForm.value.email, this.myForm.value.password, this.myForm.value.firstName, this.myForm.value.lastName));
 	}
 
+	public onSubmit() {}
 }
