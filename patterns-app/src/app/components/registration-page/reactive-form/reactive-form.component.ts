782f8bd8fe1094aa6d0ddc5589111569dab7b176
yarynalazurko <yaryna1603@gmail.com>
2020-05-09 16:13:47 +0300

    Rewrite form with FormBuilder

--- patterns-app/src/app/components/registration-page/reactive-form/reactive-form.component.ts
+++ patterns-app/src/app/components/registration-page/reactive-form/reactive-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { RegistrationPageService } from 'src/app/services/registration-page.service';
 import { UserData } from 'src/app/models/user-data.class';
 
@@ -9,22 +9,32 @@ import { UserData } from 'src/app/models/user-data.class';
 	styleUrls: ['./reactive-form.component.scss']
 })
 export class ReactiveFormComponent {
-	public myForm: FormGroup = new FormGroup({
-		email: new FormControl('', [
-			Validators.required,
-			Validators.email
-		]),
-		password: new FormControl('', Validators.required),
-		firstName: new FormControl('', Validators.required),
-		lastName: new FormControl('', Validators.required)
-	});
+	// public myForm: FormGroup = new FormGroup({
+	// 	email: new FormControl('', [
+	// 		Validators.required,
+	// 		Validators.email
+	// 	]),
+	// 	password: new FormControl('', Validators.required),
+	// 	firstName: new FormControl('', Validators.required),
+	// 	lastName: new FormControl('', Validators.required)
+	// });
+	public myForm: FormGroup;
 
-	constructor(private registrationPageService: RegistrationPageService) { }
+	constructor(
+		private registrationPageService: RegistrationPageService,
+		private formBuilder: FormBuilder) {
+		this.myForm = formBuilder.group({
+			email: ['', [Validators.required, Validators.email]],
+			password: ['', [Validators.required]],
+			firstName: ['', [Validators.required]],
+			lastName: ['', Validators.required],
+		});
+	}
 
 	public addUser() {
 		this.registrationPageService.addUser(
 			new UserData(this.myForm.value.email, this.myForm.value.password, this.myForm.value.firstName, this.myForm.value.lastName));
 	}
 
-	public onSubmit() {}
+	public onSubmit() { }
 }
